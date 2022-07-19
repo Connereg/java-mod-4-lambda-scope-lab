8fa1c5b6e45308efc1836c6d394de51c98abfc00
Conner Gleason <connereg@optonline.net>
2022-07-19 14:54:54 -0400

    Added Comments for understanding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import java.util.function.UnaryOperator;
 
 public class Main {
-    static String PREFIX = "__prefix__";
-    static String SUFFIX = "__suffix__";
+    static String PREFIX = "__prefix__";  // CAN ONLY BE USED IN UNARYOPERATOR IF IT IS DESIGNED AS STATIC, NOT INSTANCE VARIABLE
+    static String SUFFIX = "__suffix__";  // CAN ONLY BE USED IN UNARYOPERATOR IF IT IS DESIGNED AS STATIC, NOT INSTANCE VARIABLE
 	// fix the operator code
-    public static UnaryOperator<String> operator = str -> {
+    public static UnaryOperator<String> operator = str -> {   // UNARYOPERATOR IS A PREDEFINED INTERFACE, OUTPUTS SAME TYPE AS INPUT
         String trimmedString = str.replaceAll("\\s","");
         return (PREFIX + trimmedString + SUFFIX);
     };
